3e49599437db1512d1e35bbd066e061111e8da33
ranudero <landerverbruggh@gmail.com>
2025-01-09 13:52:00 +0100

    feat: create the DB needed for event

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,6 +1,22 @@
-CREATE TABLE attendees (
+CREATE TABLE Attendees (
     id INT generated always as IDENTITY,
     name VARCHAR(255) NOT NULL,
     code VARCHAR(4) NOT NULL,
     PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE Event (
+    id INT generated always as IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    start DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE InvitePerEvent (
+    id INT generated always as IDENTITY,
+    event_id INT NOT NULL,
+    attendee_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (event_id) REFERENCES event(id),
+    FOREIGN KEY (attendee_id) REFERENCES attendees(id)
+);
